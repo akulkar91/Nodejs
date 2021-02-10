b9f6be41a93d21d4421571ca18b8e117b62ff06d
karthik akula <karthikakula1991@gmail.com>
2021-02-10 02:46:39 -0500

    Brief introduction to express js

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const http = require('http');
 
-const {handler, someText} = require('./routes');
+const express = require('express');
 
-const server = http.createServer(handler);
+const app = express();
 
+// use method will hook the middlware to express js 
+//which is responsible to listen to the server requests
+app.use((req, res, next) => { //first middlware
+    console.log("I'm in the first middleware");
+    next(); // by executing this method express will explicitly look for next middleware in the code
+    // else it would not do anything 
+    // we can send the response back instead 
+    // next method should be invoked only before sending the response back
+});
 
-server.listen(3000);
+app.use((req, res, next) => { //second middleware
+    console.log("I'm in the second middleware");
+    // send
+    // // res.write('<h1>Hi from 1st express</h1>');
+    // res.write('<h1>Hello from express</h1>');
+    res.send('<h2>end</h2>');
+});
+
+// const server = http.createServer(app);
+
+// server.listen(3000);
+
+app.listen(3000);
